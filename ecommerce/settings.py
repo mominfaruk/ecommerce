30123fee8fb77268474e377d007ec70475367918
Faruk <hilton627681@gmail.com>
2023-09-11 07:14:59 +0600

    changes in settings and requirements file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ak8zcii2zTmSW2qEuBVH',
+        'HOST': 'containers-us-west-123.railway.app',
+        'PORT': '7028',
     }
 }
 
